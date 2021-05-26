3e88a51379e5a2478186fd69b345a36d2d5f1f8a
As'ad Saleh Umar <azzzad.s@gmail.com>
2021-05-26 11:43:19 +0700

    feat: create performance review form

--- src/screens/PerformanceReviewDetailScreen.tsx
+++ src/screens/PerformanceReviewDetailScreen.tsx
@@ -1,18 +1,58 @@
 export default function PerformanceReviewDetailScreen() {
+  function handleSubmit(e: any) {
+    e.preventDefault();
+    console.log(e.target);
+  }
   return (
     <div>
-      <form>
-        <label>Can get the job done well.</label>
-        <select></select>
+      <h2>Performance Review for: USER</h2>
+      <h4>By: USER_ME</h4>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>1. Can get the job done well.</label>
+          <select>
+            <option>1. Bad</option>
+            <option>2. Poor</option>
+            <option>3. Fine</option>
+            <option>4. Good</option>
+            <option>5. Excelent</option>
+          </select>
+        </div>
 
-        <label>Team Work</label>
-        <select></select>
+        <div>
+          <label>2. Team Work</label>
+          <select>
+            <option>1. Bad</option>
+            <option>2. Poor</option>
+            <option>3. Fine</option>
+            <option>4. Good</option>
+            <option>5. Excelent</option>
+          </select>
+        </div>
 
-        <label>Communication</label>
+        <div>
+          <label>3. Communication</label>
+          <select>
+            <option>1. Bad</option>
+            <option>2. Poor</option>
+            <option>3. Fine</option>
+            <option>4. Good</option>
+            <option>5. Excelent</option>
+          </select>
+        </div>
 
-        <label>Time Dicipline</label>
+        <div>
+          <label>4. Time Dicipline</label>
+          <select>
+            <option>1. Bad</option>
+            <option>2. Poor</option>
+            <option>3. Fine</option>
+            <option>4. Good</option>
+            <option>5. Excelent</option>
+          </select>
+        </div>
 
-        <label></label>
+        <input type="submit" />
       </form>
     </div>
   );
