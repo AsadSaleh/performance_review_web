b382c9623c24d7979fc58d73c79f3ff0c29b0032
As'ad Saleh Umar <azzzad.s@gmail.com>
2021-05-30 18:56:03 +0700

    feat(api): allow cors

--- api/index.ts
+++ api/index.ts
@@ -5,6 +5,16 @@ import employeeRoute from "./routes/employee";
 const app = express();
 const port = 3000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send({
     ping: "PONG",
