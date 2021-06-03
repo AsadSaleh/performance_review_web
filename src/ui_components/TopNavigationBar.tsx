c3b709fbe525b1ff94b6360aa1c67b4e01867350
As'ad Saleh Umar <azzzad.s@gmail.com>
2021-06-03 21:57:02 +0700

    feat: style TopNavigationBar

--- src/ui_components/TopNavigationBar.tsx
+++ src/ui_components/TopNavigationBar.tsx
@@ -1,42 +1,201 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { BellIcon, MenuIcon, XIcon } from "@heroicons/react/outline";
+import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
 import { useAuth } from "../store/auth";
+
+const navigationMenus = [
+  { label: "Home", path: "/" },
+  { label: "Employee", path: "/employee" },
+  { label: "Performance Reviews", path: "/performance-review" },
+  { label: "Pending Performance Reviews", path: "/pending-performance-review" },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function TopNavigationBar() {
   const {
     state: { user },
     dispatch,
   } = useAuth();
-  // const history = useHistory();
+
   function handleLogout() {
     dispatch({
       type: "logout",
     });
   }
+  const profileMenus = [{ label: "Sign out", onClick: handleLogout }];
 
   return (
-    <div className="px-2">
-      <Link className="mx-1" to="/">
-        Home
-      </Link>
-      {user?.role === "admin" ? (
-        <>
-          <Link className="mx-1" to="/employee">
-            Employees
-          </Link>
-          <Link className="mx-1" to="/performance-review">
-            Performance Reviews
-          </Link>
-        </>
-      ) : null}
-      <Link className="mx-1" to="/pending-performance-review">
-        Pending Performance Reviews
-      </Link>
+    <div>
+      <Disclosure as="nav" className="bg-gray-800">
+        {({ open }) => (
+          <>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="flex items-center justify-between h-16">
+                <div className="flex items-center">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="h-8 w-8"
+                      src="https://i0.wp.com/blog.paypay.ne.jp/wp-content/uploads/2019/03/cropped-logo_512_512.png?ssl=1"
+                      alt="Workflow"
+                    />
+                  </div>
+                  <div className="hidden md:block">
+                    <div className="ml-10 flex items-baseline space-x-4">
+                      {navigationMenus.map((item, itemIdx) => (
+                        <Fragment key={item.label}>
+                          <NavLink
+                            to={item.path}
+                            activeClassName="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                            className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            exact
+                          >
+                            {item.label}
+                          </NavLink>
+                        </Fragment>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+                <div className="hidden md:block">
+                  <div className="ml-4 flex items-center md:ml-6">
+                    {/* <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">View notifications</span>
+                      <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    </button> */}
+                    <div className="ml-3">
+                      <div className="text-base font-medium leading-none text-white text-right">
+                        {user?.name}
+                      </div>
+                      <div className="text-sm font-medium leading-none text-gray-400">
+                        {user?.email}
+                      </div>
+                    </div>
 
-      <span>Hello {user?.name}</span>
-      <button onClick={handleLogout}>
-        <span>Logout</span>
-      </button>
+                    {/* Profile dropdown */}
+                    <Menu as="div" className="ml-3 relative">
+                      {({ open }) => (
+                        <>
+                          <div>
+                            <Menu.Button className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                              <span className="sr-only">Open user menu</span>
+                              <img
+                                className="h-8 w-8 rounded-full"
+                                src="https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                                // src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                alt=""
+                              />
+                            </Menu.Button>
+                          </div>
+                          <Transition
+                            show={open}
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                          >
+                            <Menu.Items
+                              static
+                              className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                            >
+                              {profileMenus.map((item) => (
+                                <Menu.Item key={item.label}>
+                                  {({ active }) => (
+                                    <a
+                                      href="#"
+                                      onClick={item.onClick}
+                                      className={classNames(
+                                        active ? "bg-gray-100" : "",
+                                        "block px-4 py-2 text-sm text-gray-700"
+                                      )}
+                                    >
+                                      {item.label}
+                                    </a>
+                                  )}
+                                </Menu.Item>
+                              ))}
+                            </Menu.Items>
+                          </Transition>
+                        </>
+                      )}
+                    </Menu>
+                  </div>
+                </div>
+                <div className="-mr-2 flex md:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="md:hidden">
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                {navigationMenus.map((item, itemIdx) => (
+                  <Fragment key={item.label}>
+                    <NavLink
+                      to={item.path}
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                      activeClassName="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                      exact
+                    >
+                      {item.label}
+                    </NavLink>
+                  </Fragment>
+                ))}
+              </div>
+              <div className="pt-4 pb-3 border-t border-gray-700">
+                <div className="flex items-center px-5">
+                  <div className="flex-shrink-0">
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src="https://images.unsplash.com/photo-1579546929662-711aa81148cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+                      // src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </div>
+                  <div className="ml-3">
+                    <div className="text-base font-medium leading-none text-white">
+                      {user?.name}
+                    </div>
+                    <div className="text-sm font-medium leading-none text-gray-400">
+                      {user?.email}
+                    </div>
+                  </div>
+                  <button className="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                    <span className="sr-only">View notifications</span>
+                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  </button>
+                </div>
+                <div className="mt-3 px-2 space-y-1">
+                  {profileMenus.map((item) => (
+                    <a
+                      key={item.label}
+                      onClick={item.onClick}
+                      href="#"
+                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700"
+                    >
+                      {item.label}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
     </div>
   );
 }
