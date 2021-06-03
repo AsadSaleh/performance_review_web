4fe5d6130cab6628c61fbb8a04ee031b58266fe2
As'ad Saleh Umar <azzzad.s@gmail.com>
2021-06-04 00:41:23 +0700

    chore: delte comments

--- src/ui_components/BaseLayout.tsx
+++ src/ui_components/BaseLayout.tsx
@@ -17,14 +17,7 @@ export default function BaseLayout({
         </div>
       </header>
       <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {/* Replace with your content */}
-          {children}
-          {/* <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
-          </div> */}
-          {/* /End replace */}
-        </div>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">{children}</div>
       </main>
     </>
   );
