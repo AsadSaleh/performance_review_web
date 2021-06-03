cfac0de3eea2c0e49e839dd13dd308edd57db8f0
As'ad Saleh Umar <azzzad.s@gmail.com>
2021-06-04 04:22:46 +0700

    fix: missing key on .map

--- src/module/adminPerfReview/PerformanceReviewDetailScreen.tsx
+++ src/module/adminPerfReview/PerformanceReviewDetailScreen.tsx
@@ -108,7 +108,10 @@ export default function PerformanceReviewDetailScreen(
                 {perfReview.PerformanceReviewAnswers.length ? (
                   <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
                     {perfReview.PerformanceReviewAnswers.map((e) => (
-                      <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">
+                      <li
+                        key={e.Question.id}
+                        className="pl-3 pr-4 py-3 flex items-center justify-between text-sm"
+                      >
                         <div className="w-0 flex-1 flex items-center">
                           <span className="ml-2 flex-1 w-0 truncate">
                             {e.Question.text}
